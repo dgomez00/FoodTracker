8aa1f2026162a55b7037c2557e18854c71489f31
Diego Gomez <dgg2010@gmail.com>
2019-09-11 14:03:25 -0700

    set up webapi

---
+++ AspNetCore/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCore
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{ }
+
+		public DbSet<FoodRecord> FoodRecords { get; set; }
+	}
+}
